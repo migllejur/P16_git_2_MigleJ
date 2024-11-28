4eeb8efd31f2ee25f4efcc515703618966b7cc7f
migllejur <migllejur@gmail.com>
2024-11-28 16:59:13 +0200

    pakeisti failai
    istrinta logging biblioteka is aritmetikos failo, istrintos neprintinamos zinutes is logging failo

--- logging_i_faila.py
+++ logging_i_faila.py
@@ -5,8 +5,6 @@ logging.basicConfig(level=logging.ERROR,
                     encoding="utf-8",
                     format="%(asctime)s || %(levelname)s || %(message)s")
 
-logging.debug("debug lygio žurnalo eilutė")  # nebus printinama
-logging.info("info lygio žurnalo eilutė")  # nebus printinama
 logging.warning("warning lygio žurnalo eilutė")
 logging.error("error lygio žurnalo eilutė")
 logging.critical("critical lygio žurnalo eilutė")
